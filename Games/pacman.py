7882b866caf0f2285bf071815aa573ea23a8f17d
gabdourado <gab.dourado.if23@gmail.com>
2025-04-25 00:53:50 -0300

    Adicionei tiles no fundo

--- Games/pacman.py
+++ Games/pacman.py
@@ -2,17 +2,6 @@
 
 import pygame
 
-AMARELO = (255, 255, 0)
-AZUL = (0, 0, 255)
-PRETO = (0, 0, 0)
-
-LARGURA = 800
-ALTURA = 600
-TAMANHO_TILE = 50
-
-VELOCIDADE = 5
-
-
 mapa = [
     [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -25,9 +14,30 @@ mapa = [
     [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+AMARELO = (255, 255, 0)
+
+VELOCIDADE = 5
+
+SIZE_TILE = 32
+
+LARGURA, ALTURA = SIZE_TILE * len(mapa[0]), SIZE_TILE * len(mapa) 
+
+pygame.init()
+
+screen = pygame.display.set_mode((LARGURA, ALTURA))
+
+tileset = pygame.image.load("./tileset-fundo.png").convert_alpha()
+
+def get_tile(x, y):
+    tile = tileset.subsurface((x * SIZE_TILE, y * SIZE_TILE, SIZE_TILE, SIZE_TILE))
+    return tile
+tiles = {
+    1: get_tile(0, 0),  # grama (área acessível)
+    0: get_tile(1, 0),  # pedra (área inacessível)
+}
 
 def inicializar():
   pygame.init()
@@ -36,18 +46,9 @@ def inicializar():
   return tela
 
 def desenhar_mapa(tela, mapa):
-  for linha in range(len(mapa)):
-    for coluna in range(len(mapa[linha])):
-      x = coluna * TAMANHO_TILE
-      y = linha * TAMANHO_TILE
-
-      if mapa[linha][coluna] == 1:
-        cor = AZUL
-      else:
-        cor = PRETO
-
-      pygame.draw.rect(tela, cor, (x, y, TAMANHO_TILE, TAMANHO_TILE))
-      pygame.draw.rect(tela, PRETO, (x, y, TAMANHO_TILE, TAMANHO_TILE), 1)
+    for y, linha in enumerate(mapa):
+        for x, tile_id in enumerate(linha):
+            tela.blit(tiles[tile_id], (x * SIZE_TILE, y * SIZE_TILE))
 
 def criar_jogador(x, y, largura, altura):
   return pygame.Rect(x, y, largura, altura)
@@ -57,15 +58,15 @@ def desenhar_tela(tela, jogador, cor):
   pygame.draw.rect(tela, cor, jogador)
   pygame.display.flip()
 
-def valida(x, y):
-  tile_coluna = x // TAMANHO_TILE
-  tile_linha = y // TAMANHO_TILE
+def sprite_valido(x, y):
+  tile_coluna = x // SIZE_TILE
+  tile_linha = y // SIZE_TILE
 
   if 0 <= tile_linha < len(mapa) and 0 <= tile_coluna < len(mapa[0]):
     return mapa[tile_linha][tile_coluna] == 0
   return False
 
-def mover_jogador(jogador, mapa):
+def mover_jogador(jogador):
   teclas = pygame.key.get_pressed()
 
   dx = dy = 0
@@ -82,10 +83,10 @@ def mover_jogador(jogador, mapa):
   novo_jogador = jogador.move(dx, dy)
 
   if (
-      valida(novo_jogador.left, novo_jogador.top) and
-      valida(novo_jogador.right - 1, novo_jogador.top) and
-      valida(novo_jogador.left, novo_jogador.bottom - 1) and
-      valida(novo_jogador.right - 1, novo_jogador.bottom - 1)
+      sprite_valido(novo_jogador.left, novo_jogador.top) and
+      sprite_valido(novo_jogador.right - 1, novo_jogador.top) and
+      sprite_valido(novo_jogador.left, novo_jogador.bottom - 1) and
+      sprite_valido(novo_jogador.right - 1, novo_jogador.bottom - 1)
     ):
     jogador.x += dx
     jogador.y += dy
@@ -99,16 +100,16 @@ def loop_jogo(tela, jogador):
       if evento.type == pygame.QUIT:
           rodando = False
 
-    mover_jogador(jogador, mapa)
+    mover_jogador(jogador)
     desenhar_tela(tela, jogador, AMARELO)
     clock.tick(60)
 
   pygame.quit()
 
 def main():
-  altura, largura = 20, 20
+  altura, largura = 10, 10
   tela = inicializar()
-  jogador = criar_jogador(400, 300, altura, largura)
+  jogador = criar_jogador(LARGURA // 2, ALTURA // 2, altura, largura)
   loop_jogo(tela, jogador)
 
 if __name__ == "__main__":
